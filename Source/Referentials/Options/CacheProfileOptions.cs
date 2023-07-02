494e6a274a80c9cd128ae09d3f6ce63c3575eacb
markchristianmenchavez <markchristian.menchavez@gmail.com>
2023-07-02 15:08:31 +0800

    Add SonarAnalyzer and fix issues

--- Source/Referentials/Options/CacheProfileOptions.cs
+++ Source/Referentials/Options/CacheProfileOptions.cs
@@ -1,12 +1,23 @@
 namespace Referentials.Options;
 
+using System.Runtime.Serialization;
 using Microsoft.AspNetCore.Mvc;
 
 /// <summary>
 /// The caching options for the application.
 /// </summary>
+[Serializable]
 #pragma warning disable CA1710 // Identifiers should have correct suffix
 public class CacheProfileOptions : Dictionary<string, CacheProfile>
 #pragma warning restore CA1710 // Identifiers should have correct suffix
 {
+    public CacheProfileOptions()
+        : base()
+    {
+    }
+
+    protected CacheProfileOptions(SerializationInfo serializationInfo, StreamingContext streamingContext)
+        : base(serializationInfo, streamingContext)
+    {
+    }
 }
